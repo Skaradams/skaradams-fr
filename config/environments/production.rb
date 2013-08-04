e4d4219ca43002629ae84294b7b2c7c2026ee8a7
Skaradams <skaradams@ubuntu.(none)>
2013-08-04 23:38:55 +0200

    css and paperclip s3

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,8 +83,7 @@ Skaradams::Application.configure do
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
-      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
-      :region => 'Ireland'
+      :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
   }
 end
