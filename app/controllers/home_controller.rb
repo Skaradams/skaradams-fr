2569e6ec8736f8f0e0b3647d1d068ed9bf9b93b7
Skaradams <skaradams@ubuntu.(none)>
2013-08-05 13:55:08 +0200

    bug bizarre

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,7 +2,7 @@ class HomeController < ApplicationController
   def index
     @categories = Category.all
     filters = params['categories'].split(',') rescue nil
-    @articles = Article.published
+    @articles = Article.all
     if filters.present?
       @articles = @articles.includes(:category).where('categories.slug in (?)', filters)
     end
