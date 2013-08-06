fc330997909a96eb8d449d059a648d77dfecbab0
Skaradams <skaradams@ubuntu.(none)>
2013-08-06 15:14:26 +0200

    sendgrid config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,4 +87,14 @@ Skaradams::Application.configure do
     },
     :s3_host_name => 's3-eu-west-1.amazonaws.com',
   }
+
+  config.action_mailer.default_url_options = { :host => 'www.skaradams.fr' }
+  ActionMailer::Base.smtp_settings = {
+    :address        => "smtp.sendgrid.net",
+    :port           => "25",
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => ENV['SENDGRID_DOMAIN']
+  }
 end
